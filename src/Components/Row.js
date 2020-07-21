4c7442ab3d0ff0fdcb149be92494a3de9353f0ad
Chirag Jain <me.chiragjain@gmail.com>
2020-07-22 01:29:52 +0530

    API Setup & Extracting Data from API

---
+++ src/Components/Row.js
@@ -0,0 +1,33 @@
+import React,{useState, useEffect} from 'react';
+import axios from './../axios';
+import "./Row.css"
+
+const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }){
+  const [movies, setMovies]=useState([]);
+
+  useEffect(()=>{
+    async function fetchData(){
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return(
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {movies.map(movie => {
+          return(
+            <img className="row_poster" src={`${imageBaseUrl}${movie.poster_path}`} alt={movie.name}/>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Row;
